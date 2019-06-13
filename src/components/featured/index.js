8830d1830f8cecdfe9c3a5baf72415cab891d920
Cherryboome <muhamidifarian@yahoo.com>
2019-06-13 14:40:42 +0300

    created slider

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Carousel from "./Carousel";
 
 const Featured = () => {
-  return <div />;
+  return (
+    <div style={{ position: "relative" }}>
+      <Carousel />
+
+      <div className='artist_name'>
+        <div className='wrapper'>Aria Grande</div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
