6c150f7c4e88e7df5ff9f1187f5d3e5bee21bff3
Cherryboome <muhamidifarian@yahoo.com>
2019-06-18 18:46:08 +0300

    completed time travel feature

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,6 +8,7 @@ class Game extends Component {
         squares: Array(9).fill(null)
       }
     ],
+    stepNumber: 0,
     xIsNext: true
   };
 
@@ -39,7 +40,7 @@ class Game extends Component {
   };
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -53,14 +54,31 @@ class Game extends Component {
           squares
         }
       ]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = this.calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -76,7 +94,7 @@ class Game extends Component {
         </div>
         <div className='game-info'>
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
